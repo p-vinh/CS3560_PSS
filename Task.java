224009386e96ef8dabab8e586603639775d03f8c
random-debug <85326677+random-debug@users.noreply.github.com>
2023-04-20 22:10:53 -0700

    Constructor For Task/Private Data

--- Task.java
+++ Task.java
@@ -1,10 +1,18 @@
 public class Task {
     
-    String name;
-    String type;
-    double time;
-    double duration;
-    int date;
+    private String name;
+    private String type;
+    private double time;
+    private double duration;
+    private int date;
+    
+    private Task(String name, String type, double time, double duration, int date) {
+        this.name = name;
+        this.type = type;
+        this.time = time;
+        this.duration = duration;
+        this.date = date;
+    }
 
     public void print() {
         System.out.println("Name: " + name);
